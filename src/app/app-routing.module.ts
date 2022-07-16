a47cdca4fa3aad0536b538771f5abd7dbe413f3b
atienoobwanda <atienoobwanda@gmail.com>
2022-07-17 01:29:37 +0300

    defining the app route structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'home', component: LandingComponent},
+  { path: '', redirectTo:"/home", pathMatch:"full"},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
