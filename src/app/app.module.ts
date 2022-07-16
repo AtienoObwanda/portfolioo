47a531dda9438ca777d98a188ec3f297a911aaf1
atienoobwanda <atienoobwanda@gmail.com>
2022-07-17 01:26:17 +0300

    setting up project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    SidebarComponent,
   ],
   imports: [
     BrowserModule,
