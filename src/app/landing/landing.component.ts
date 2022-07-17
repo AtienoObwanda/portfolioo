c1fed3b9a1341618c702f20101fde69914b3018d
atienoobwanda <atienoobwanda@gmail.com>
2022-07-17 16:54:22 +0300

    adding contact form

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,34 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { ContactService } from '../contact.service';
+import { FormGroup, FormBuilder, FormControl, Validators, NgForm } from '@angular/forms'
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.css']
+  styleUrls: ['./landing.component.css'],
+  providers: [ContactService],
   
 })
 export class LandingComponent implements OnInit {
   prec : any;
   suiv:any;
+  FormData: any;
+  // FormData: FormGroup;
 
-constructor() { }
 
-  ngOnInit(): void {
-    this.prec()
-    this.suiv()
-    // var swiper = new Swiper('.blog-slider', {
-    //   spaceBetween: 30,
-    //   effect: 'fade',
-    //   loop: true,
-    //   mousewheel: {
-    //     invert: false,
-    //   },
-    //   // autoHeight: true,
-    //   pagination: {
-    //     el: '.blog-slider__pagination',
-    //     clickable: true,
-    //   }
-    // });
-    
+  // constructor(private builder: FormBuilder) { }
+  constructor(private builder: FormBuilder, private contact: ContactService) { }
+
+
+  ngOnInit() {
+    this.FormData = this.builder.group({
+      Fullname: new FormControl('', [Validators.required]),
+      Email: new FormControl('', [Validators.required, Validators.email]),
+      Comment: new FormControl('', [Validators.required])
+    });
   }
 
+
+  onSubmit(FormData: any) {
+    console.log(FormData)
+    this.contact.PostMessage(FormData)
+      .subscribe(response => {
+        location.href = 'https://mailthis.to/confirm'
+        console.log(response)
+      }, error => {
+        console.warn(error.responseText)
+        console.log({ error })
+      })
+  }
 }
+
+
+
+
