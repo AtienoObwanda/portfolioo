315707d0b2f2669ace9887c05c0bde8258e6c901
atienoobwanda <atienoobwanda@gmail.com>
2022-07-27 14:09:43 +0300

    restore app

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-app.use(express.static(__dirname + '/dist/portfolioo'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/portfolioo/index.html'));});
-app.listen(process.env.PORT || 8080);
