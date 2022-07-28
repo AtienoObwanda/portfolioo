d804aa9271037ebfe5fd1a93f4b77dd32974f794
atienoobwanda <atienoobwanda@gmail.com>
2022-07-28 03:22:11 +0300

    adding a service to fetch portfolio details in  the case study page

--- src/app/case-study/case-study.component.ts
+++ src/app/case-study/case-study.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ContactService } from '../contact.service';
 import { FormGroup, FormBuilder, FormControl, Validators, NgForm } from '@angular/forms'
 import { Router, ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-case-study',
@@ -13,19 +15,26 @@ import { Router, ActivatedRoute } from '@angular/router';
 export class CaseStudyComponent implements OnInit {
 
   FormData: any;
-  // FormData: FormGroup;
-
+  project:any;
 
   // constructor(private builder: FormBuilder) { }
-  constructor(private builder: FormBuilder, private contact: ContactService, private router: Router,) { }
+  constructor(private builder: FormBuilder, private contact: ContactService, private router: Router, private activatedRoute: ActivatedRoute ,private http: HttpClient) { }
 
 
   ngOnInit() {
+    let id = this.activatedRoute.snapshot.paramMap.get("id");
+    console.log(id)
+    this.project = this.http.get("https://pfolioapi.herokuapp.com/portfolio/project-detail/"+id+"/").subscribe(
+    data => this.project = data )
+
     this.FormData = this.builder.group({
       Fullname: new FormControl('', [Validators.required]),
       Email: new FormControl('', [Validators.required, Validators.email]),
       Comment: new FormControl('', [Validators.required])
     });
+
+  
+
   }
 
 
